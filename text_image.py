673c4fdf6673e321a85669e5d6b823b02d0250bc
sajjad rahman <67529599+sajjadrahman56@users.noreply.github.com>
2023-12-26 03:45:01 +0600

    Update text_image.py

--- text_image.py
+++ text_image.py
@@ -1,51 +1,51 @@
-from dotenv import load_dotenv
-load_dotenv()  
+# from dotenv import load_dotenv
+# load_dotenv()  
 
-import streamlit as st
-import os
-import pathlib
-import textwrap
-from PIL import Image
-
-
-import google.generativeai as genai
+# import streamlit as st
+# import os
+# import pathlib
+# import textwrap
+# from PIL import Image
 
 
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-## Function to load OpenAI model and get respones
-
-def get_gemini_response(input,image):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    if input!="":
-       response = model.generate_content([input,image])
-    else:
-       response = model.generate_content(image)
-    return response.text
-
-##initialize our streamlit app
-
-st.set_page_config(page_title="Gemini Image Demo")
-
-st.header("You and Me ! Here we go !! ")
-
-input = st.text_input("Enter your question here", key="unique_input_key")
-uploaded_file = st.file_uploader("select an image...", type=["jpg", "jpeg", "png"])
-
-image=""   
-
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+# import google.generativeai as genai
 
 
-submit=st.button("Tell me about the image")
+# os.getenv("GOOGLE_API_KEY")
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## If ask button is clicked
+# ## Function to load OpenAI model and get respones
 
-if submit:
+# def get_gemini_response(input,image):
+#     model = genai.GenerativeModel('gemini-pro-vision')
+#     if input!="":
+#        response = model.generate_content([input,image])
+#     else:
+#        response = model.generate_content(image)
+#     return response.text
+
+# ##initialize our streamlit app
+
+# st.set_page_config(page_title="Gemini Image Demo")
+
+# st.header("You and Me ! Here we go !! ")
+
+# input = st.text_input("Enter your question here", key="unique_input_key")
+# uploaded_file = st.file_uploader("select an image...", type=["jpg", "jpeg", "png"])
+
+# image=""   
+
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+# submit=st.button("Tell me about the image")
+
+# ## If ask button is clicked
+
+# if submit:
     
-    response=get_gemini_response(input,image)
-    st.subheader("The Response is")
-    st.write(response)
+#     response=get_gemini_response(input,image)
+#     st.subheader("The Response is")
+#     st.write(response)
