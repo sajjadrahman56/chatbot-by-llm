305d59c3c0e2825e49156a662e94d0705e436075
sajjadrahman56 <maxsajjad29@gmail.com>
2023-12-26 02:28:55 +0600

    my q and a

---
+++ app.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os 
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel("gemini-pro")
+
+def get_response(input_text):
+     response = model.generate_content(input_text)
+     return response.text
+
+st.set_page_config(page_title="Q & A", page_icon=":gem:")
+st.header("Gemini LLM application")
+input = st.text_input("Enter your question here", key="input")
+
+submit = st.button("Submit")
+
+
+if submit:
+     response = get_response(input)
+     st.subheader("The Response is :")
+     st.write(response)
