df7b229d0912e1495083c2e386abb0696f3428ea
sajjad rahman <67529599+sajjadrahman56@users.noreply.github.com>
2023-12-27 23:40:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,46 @@
 from dotenv import load_dotenv
-load_dotenv()  
-
 import streamlit as st
-import os
-import pathlib
-import textwrap
 from PIL import Image
-
-
 import google.generativeai as genai
+import os
+load_dotenv()
 
-
-os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
+model = genai.GenerativeModel("gemini-pro-vision")
 
-def get_gemini_response(input,image):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    if input!="":
-       response = model.generate_content([input,image])
+def get_gemini_response(input, image):
+    if image is None:
+        st.warning("Please upload an image.")
+        return None
+    
+    if input != "":
+        response = model.generate_content([input, image])
     else:
-       response = model.generate_content(image)
-    return response.text
+        response = model.generate_content(image)
+    
+    return response.text if response else None
 
-##initialize our streamlit app
+st.set_page_config(page_title="Here we go!", page_icon=":gem:")
 
-st.set_page_config(page_title="Gemini Image Demo")
+st.header("You and Me! Here we go!")
 
-st.header("You and Me ! Here we go !! ")
+input_text = st.text_input("Enter your question here", key="unique_input_key")
+uploaded_file = st.file_uploader("Select an image...", type=["jpg", "jpeg", "png"])
 
-input = st.text_input("Enter your question here", key="unique_input_key")
-uploaded_file = st.file_uploader("select an image...", type=["jpg", "jpeg", "png"])
-
-image=""   
+image = None
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
+submit = st.button("Describe about the image")
 
-submit=st.button("Tell me about the image")
+if submit:
+    response = get_gemini_response(input_text, image)
+    if response is not None:
+        st.subheader("Answer:> ")
+        st.write(response)
 
-## If ask button is clicked
-
-if submit:   
-    response=get_gemini_response(input,image)
-    st.subheader("The Response is")
-    st.write(response)
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# import streamlit as st
-# import os 
-# import google.generativeai as genai
-
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
- model = genai.GenerativeModel("gemini-pro")
-
- def get_response(input_text):
-      response = model.generate_content(input_text)
-      return response.text
-
- st.set_page_config(page_title="Q & A", page_icon=":gem:")
-
- st.header("Here you GO...!")
- input = st.text_input("Enter your question here", key="input")
-
- input = st.text_input("Enter your question here", key="input")
- submit = st.button("Submit")
-
-
- if submit:
-      response = get_response(input)
-      st.subheader("The Response is :")
-      st.write(response)
+ 
+ 
